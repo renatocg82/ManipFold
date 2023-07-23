b0cdd6115d7691a173a2d21a7f196ee8b851020b
renatocg82 <66907169+renatocg82@users.noreply.github.com>
2023-07-23 12:25:54 -0300

    Manipulating Folders

---
+++ src/Program/AppFolders.java
@@ -0,0 +1,47 @@
+package Program;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AppFolders {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scString = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter folder path: ");
+		String strPath = scString.nextLine();
+
+		File path = new File(strPath);
+
+		File[] folders = path.listFiles(File::isDirectory);
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("-----------------");
+		System.out.println("Folders list:");
+		System.out.println();
+
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
+
+		System.out.println("-----------------");
+		System.out.println("Files list: ");
+		System.out.println();
+
+		for (File file : files) {
+			System.out.println(file);
+		}
+
+		System.out.println("_________________");
+		
+		boolean success = new File(strPath + "\\dirTest2").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		
+		
+		scString.close();
+		sc.close();
+	}
+
+}
